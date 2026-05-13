d2ea6b1ca21215e08ee4f2c75c5a9e36094248a0
schultz-dev0 <nikola454237@gmail.com>
2026-05-13 01:11:34 +0100

    screenshot tool & sliders fix

--- .config/hypr/source/bindings.lua
+++ .config/hypr/source/bindings.lua
@@ -75,7 +75,7 @@ hl.bind(
 -- ── Utilities ─────────────────────────────────────────────────────────────────
 
 hl.bind("SHIFT + Print", hl.dsp.exec_cmd("hyprcap shot region -z -w"), { desc = "Screenshot region and save" })
-hl.bind("Print", hl.dsp.exec_cmd("hyprcap shot region -z -c -n"), { desc = "Screenshot to clipboard" })
+hl.bind("Print", hl.dsp.exec_cmd(scripts .. "/screenshot/capture.sh"), { desc = "Screenshot popup" })
 hl.bind("ALT + Print", hl.dsp.exec_cmd("hyprcap rec region -c -w"), { desc = "Start screen recording" })
 hl.bind(mainMod .. " + Print", hl.dsp.exec_cmd("hyprpicker -a || pkill hyprpicker"), { desc = "Colour picker" })
 hl.bind(
