c0f092229ef4d24dab55535e2480e97070fc0655
schultz-dev0 <nikola454237@gmail.com>
2026-05-20 19:26:31 +0100

    cloud center changes

---
+++ cloudyy_scripts/cloud-center-v2/decoration:shadow:render_power/user-configs/user_lookandfeel.lua
@@ -0,0 +1,12 @@
+-- Cloud Center user override file for lookandfeel configuration.
+-- @cloud-center-state = {"decoration:shadow:range": "30"}
+
+require("source.lookandfeel")
+
+hl.config({
+    decoration = {
+        shadow = {
+            range = 30,
+        },
+    },
+})
